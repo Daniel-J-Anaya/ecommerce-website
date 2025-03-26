e563a5fe36acacb63092e82489029263d6657e0d
Daniel-J-Anaya <ana15001@byui.edu>
2025-03-26 13:55:56 -0600

    Inital setup for Mobile View

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,15 +4,27 @@ import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import close_icon from '../Assets/cart_cross_icon.png'
+import menu_icon from '../Assets/menu_icon.png'
 
 export const Navbar = () => {
 
   const [menu, setMenu] = useState("shop");
   const {getTotalCartItems} = useContext(ShopContext);
 
+  function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  };
+
+  function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  };
+
   return (
     <div className='navbar'>
-      <li onClick={()=>{setMenu("shop")}} id='nav-logo'>
+      <div onClick={()=>{setMenu("shop")}} id='nav-logo'>
         <Link style={{ textDecoration: 'none' }} to='/'>
           <div className="nav-logo">
             <img src={logo} alt="Logo" />
@@ -20,13 +32,22 @@ export const Navbar = () => {
           </div>
         </Link>
         {menu==="shop"?<hr/>:<></>}
-      </li>
-      <ul className="nav-menu">
+      </div>
+      <ul className="nav-menu hideOnMobile">
         <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("men")}}><Link style={{ textDecoration: 'none' }} to='/men'>Men</Link>{menu==="men"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("women")}}><Link style={{ textDecoration: 'none' }} to='/women'>Women</Link>{menu==="women"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
       </ul>
+      <ul className="sidebar">
+        <img className='close-icon' src={close_icon} onClick={hideSidebar} alt='close'/>
+        <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("men")}}><Link style={{ textDecoration: 'none' }} to='/men'>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("women")}}><Link style={{ textDecoration: 'none' }} to='/women'>Women</Link>{menu==="women"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        <div className='nav-login-cart'><Link to='/login'><button>Login</button></Link></div>
+      </ul>
+      <img className='menu-icon' src={menu_icon} alt="menu" onClick={showSidebar}/>
       <div className="nav-login-cart">
         <Link to='/login'><button>Login</button></Link>
         <Link to='/cart'><img src={cart_icon} alt="Cart Icon" /></Link>
